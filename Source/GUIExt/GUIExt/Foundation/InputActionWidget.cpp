3f99dea2c7bea04b127a1f80732bef2e5b8b1249
owoDra <85442566+owoDra@users.noreply.github.com>
2024-03-04 04:54:18 +0900

    ウィジェット追加

---
+++ Source/GUIExt/GUIExt/Foundation/InputActionWidget.cpp
@@ -0,0 +1,132 @@
+﻿// Copyright (C) 2023 owoDra
+
+#include "InputActionWidget.h"
+
+#include "Components/Image.h"
+#include "InputAction.h"
+#include "PlayerMappableKeySettings.h"
+#include "CommonUITypes.h"
+#include "CommonInputSubsystem.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(InputActionWidget)
+
+/////////////////////////////////////////////////////////////////////////
+
+UInputActionWidget::UInputActionWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+
+// Initialization
+
+void UInputActionWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (auto* InputSubsystem{ GetInputSubsystem() })
+	{
+		InputSubsystem->OnInputMethodChangedNative.AddUObject(this, &ThisClass::HandleInputMethodChanged);
+
+		HandleInputMethodChanged(InputSubsystem->GetCurrentInputType());
+	}
+}
+
+void UInputActionWidget::NativeDestruct()
+{
+	if (auto* InputSubsystem{ GetInputSubsystem() })
+	{
+		InputSubsystem->OnInputMethodChangedNative.RemoveAll(this);
+	}
+
+	Super::NativeDestruct();
+}
+
+
+// Input Method
+
+void UInputActionWidget::HandleInputMethodChanged(ECommonInputType NewInputMethod)
+{
+	if (NewInputMethod == ECommonInputType::Gamepad)
+	{
+		UpdateKey(GamepadAction, NewInputMethod);
+	}
+	else if (NewInputMethod == ECommonInputType::Touch)
+	{
+		UpdateKey(TouchAction, NewInputMethod);
+	}
+	else
+	{
+		UpdateKey(KeyboardAction, NewInputMethod);
+	}
+}
+
+
+// Display Key
+
+void UInputActionWidget::UpdateKey(const FInputActionWidgetData& Data, ECommonInputType NewInputMethod)
+{
+	if (!CommonUI::IsEnhancedInputSupportEnabled())
+	{
+		return;
+	}
+
+	auto NewKey{ GetKeyOfSlot(Data) };
+	NewKey = NewKey.IsValid() ? NewKey : CommonUI::GetFirstKeyForInputType(GetOwningLocalPlayer(), NewInputMethod, Data.InputAction);
+
+	if (NewKey.IsValid())
+	{
+		if (auto* Resource{ GetIconForKey(NewKey).GetResourceObject() })
+		{
+			Image_Key->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+			Image_Key->SetBrushResourceObject(Resource);
+			return;
+		}
+	}
+
+	Image_Key->SetVisibility(ESlateVisibility::Collapsed);
+}
+
+
+FKey UInputActionWidget::GetKeyOfSlot(const FInputActionWidgetData& Data) const
+{
+	auto MappingName{ Data.SpecificMappingName };
+	if (MappingName.IsNone())
+	{
+		auto& InputAction{ Data.InputAction };
+		auto Mappable{ InputAction ? InputAction->GetPlayerMappableKeySettings() : nullptr };
+
+		MappingName = Mappable ? Mappable->GetMappingName() : MappingName;
+	}
+
+	const auto* LocalPlayer{ GetOwningLocalPlayer() };
+	const auto* EISubsystem{ ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer) };
+	auto* UserSettings{ EISubsystem ? EISubsystem->GetUserSettings() : nullptr };
+	const auto* PrimaryMaping{ UserSettings->FindCurrentMappingForSlot(MappingName, Data.Slot) };
+
+	return PrimaryMaping ? PrimaryMaping->GetCurrentKey() : FKey();
+}
+
+FSlateBrush UInputActionWidget::GetIconForKey(FKey InKey) const
+{
+	FSlateBrush SlateBrush;
+
+	auto* CommonInputSubsystem{ ULocalPlayer::GetSubsystem<UCommonInputSubsystem>(GetOwningLocalPlayer()) };
+	auto* InputPlatformSettings{ UCommonInputPlatformSettings::Get() };
+
+	if (CommonInputSubsystem && InputPlatformSettings)
+	{
+		auto GamepadName{ CommonInputSubsystem->GetCurrentGamepadName() };
+		GamepadName = GamepadName.IsNone() ? InputPlatformSettings->GetDefaultGamepadName() : GamepadName;
+
+		InputPlatformSettings->TryGetInputBrush(
+			SlateBrush,
+			InKey,
+			InKey.IsGamepadKey() ? ECommonInputType::Gamepad : ECommonInputType::MouseAndKeyboard,
+			GamepadName);
+	}
+
+	return SlateBrush;
+}
---
+++ Source/GUIExt/GUIExt/Foundation/InputActionWidget.h
@@ -0,0 +1,86 @@
+﻿// Copyright (C) 2023 owoDra
+
+#pragma once
+
+#include "CommonUserWidget.h"
+
+#include "UserSettings/EnhancedInputUserSettings.h"
+
+#include "InputActionWidget.generated.h"
+
+class UInputAction;
+class UImage;
+
+
+/**
+ * InputAction data to be referenced
+ */
+USTRUCT(BlueprintType)
+struct FInputActionWidgetData
+{
+	GENERATED_BODY()
+public:
+	FInputActionWidgetData() = default;
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TObjectPtr<const UInputAction> InputAction{ nullptr };
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FName SpecificMappingName{ NAME_None };
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	EPlayerMappableKeySlot Slot{ EPlayerMappableKeySlot::First };
+};
+
+
+/**
+ * Widget for displaying input action key icon
+ */
+UCLASS(Abstract, Blueprintable)
+class GUIEXT_API UInputActionWidget : public UCommonUserWidget
+{
+	GENERATED_BODY()
+public:
+	UInputActionWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+
+	////////////////////////////////////////////////////////////
+	// Initialization
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+
+	////////////////////////////////////////////////////////////
+	// Input Method
+protected:
+	void HandleInputMethodChanged(ECommonInputType NewInputMethod);
+
+
+	////////////////////////////////////////////////////////////
+	// Display Key
+protected:
+	UPROPERTY(EditAnywhere, Category = "Input Action")
+	FInputActionWidgetData KeyboardAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input Action")
+	FInputActionWidgetData GamepadAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input Action")
+	FInputActionWidgetData TouchAction;
+
+protected:
+	virtual void UpdateKey(const FInputActionWidgetData& Data, ECommonInputType NewInputMethod);
+
+	virtual FKey GetKeyOfSlot(const FInputActionWidgetData& Data) const;
+	virtual FSlateBrush GetIconForKey(FKey InKey) const;
+
+
+	////////////////////////////////////////////////////////////
+	// Widget Bounds
+private:	
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, BlueprintProtected = true, AllowPrivateAccess = true))
+	TObjectPtr<UImage> Image_Key;
+
+};
